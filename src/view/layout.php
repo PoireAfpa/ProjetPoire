7e11b02c53433d0cde65cbbdc02f1964b16a852c
Sttayllah <marielorraine.collignon@gmail.com>
2022-03-02 20:36:58 +0100

    Ajout d'un footer à layout dans src/view-création d'une page d'accueil (index.php) dans src/view

--- src/view/layout.php
+++ src/view/layout.php
@@ -14,7 +14,7 @@
 <body style="background-color: #d8dbdd;">
 
 <header>
-<nav class="navbar  navbar-expand-lg sticky-top navbar-light bg-light ">
+<nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light ">
     <!-- TO DO AJOUTER LOGO
     <img src="" alt="" width="30" height="24">-->
   <a class="navbar-brand" href="">ABI</a>
@@ -46,19 +46,49 @@
     </ul>
 </nav>
 </header>
-<hr>
 
 
 
-<footer>
-<footer class="bg-light text-center text-lg-start">
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: #8fcdf6;">
-    © 2022 Copyright:
-    <a class="text-dark" href="https://github.com/PoireAfpa/ProjetPoire">Equipe Poire</a>
+
+
+<footer class="bg-light text-center fixed-bottom text-lg-start">
+<div class="container">
+  <div class="row">
+    <div class="col">
+        <h4 class="fs-5 text p-1">Adresse</h4>
+            
+                <div class="place">
+                    <span class="fas fa-map-marker-alt"></span>
+                    <span class="text">39 Local Host </span>
+                    <span class="text">99999 VERGERS</span>
+                </div>  
+                <div class="phone">
+                    <span class="fas fa-phone-alt"></span>
+                    <span class="text">09-09-09-09-09</span>
+                </div>
+                <div class="email">
+                    <span class="fas fa-envelope"></span>
+                    <span class="text">poire.afpa@gmail.com</span>
+                </div>
+    </div>
+    <div class="col">
+    <h4 class="text-center fs-5 text p-1">A propos de nous</h4>
+                <div class="text-center fs-6 text p-1">Lorem idivsum dolor sit amet consectetur adipisicing elit. Corrupti exercitationem cum dolores soluta 
+                    repudiandae corporis asperiores, tenetur repellendus dolore quibusdam quam vitae illo amet? 
+          </div>
+    </div>
+    <div class="col text-end ">
+      <h4 class="fs-5 text p-1">Géolocalisation à venir</h4>
+    </div>
   </div>
+</div>
   <!-- Copyright -->
-</footer>
+        <div class="text-center p-3" style="background-color: #8fcdf6;">
+    © 2022 Copyright:
+            <a class="text-dark" href="https://github.com/PoireAfpa/ProjetPoire">Equipe Poire</a>
+        </div>
+  <!-- Copyright -->
+    </div>
 </footer>
 
 </body>
