9cb3bd7eb69d62ccfb7d4c5316ffc0f40b4878bc
NaimaOula <n.coton@gmail.com>
2022-03-04 11:51:35 +0100

    Users model cleaned up

--- src/model/users.php
+++ src/model/users.php
@@ -1,87 +1,105 @@
 <?php
 
-namespace App\view\model;
+namespace App\model;
+
 use App\core\Dao;
 use App\core\Model;
 
+class User extends Model
+{
 
-class User extends Model{
-
-private $id;
-private $email;
-private $password;
+// déclarer les propriétés
+private $idUser;
+private $loginUser;
+private $passUser;
 private $role;
 
-
-
-/**
- * Get the value of id
- */
-public function getId()
-{
-return $this->id;
-}
-
-
-/**
- * Get the value of email
- */
-public function getEmail()
-{
-return $this->email;
-}
-
-/**
- * Set the value of email
- */
-public function setEmail($email):void
-{
-$this->email = $email;
-
-
-}
-
-/**
- * Get the value of password
- */
-public function getPassword()
-{
-return $this->password;
-}
-public function beforeInsertInSession()
-{
-    unset($this->password);
-}
-
-public function setPassword(string $password): void
-{
-    if (strlen($password) <6 )
+    /**
+     * @return mixed
+     */
+    public function getIdUser()
     {
-        throw new \Exception("Le mot de passe est trop court");
+        return $this->idUser;
     }
-    $this->password = password_hash($password, PASSWORD_BCRYPT);
-}
 
-public function getOneByMail($email):?User  {
- $user=Dao::getOne(self::class, ["email"=>$email]);
- if ($user==false){
-    $user=null;
-   
-}
+    /**
+     * @return mixed
+     */
+    public function getLoginUser()
+    {
+        return $this->loginUser;
+    }
 
-return $user;
-}
+    /**
+     * @param mixed $username
+     */
+    public function setLoginUser($loginUser): void
+    {
+        $this->loginUser = $loginUser;
+    }
 
-public function insert(){
-    Dao::insertOne($this,get_object_vars($this));
-    
-}
+    /**
+     * @return mixed
+     */
+    public function getPassUser()
+    {
+        return $this->passUser;
+    }
 
-/**
- * Get the value of role
- */
-public function getRole()
-{
-return $this->role;
-}
+    public function beforeInsertInSession()
+    {
+        unset($this->passUser);
+    }
+
+    /**
+     * @param mixed $passUser
+     */
+    public function setPassUser(string $passUser): void
+    {
+        if (strlen($passUser) <6 )
+        {
+            throw new \Exception("Le mot de passe est trop court");
+        }
+        $this->passUser = password_hash($passUser, PASSWORD_BCRYPT);
+    }
+
+        /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role): void
+    {
+        $this->role = $role;
+    }
+
+    public function getAll()
+    {
+
+        $users = Dao::getMany(self::class);
+        return $users;
+    }
+
+    public function getOneById(int $idUser)
+    {
+
+        $user = Dao::getOne(self::class,
+            [
+                'idUser' => $idUser
+            ]);
+        return $user;
+    }
+
+    /* 
+    public function insert(){
+        Dao::insertOne($this,get_object_vars($this));
+    }
+    */
+
 }
