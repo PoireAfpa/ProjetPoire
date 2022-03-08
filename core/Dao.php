e0bcae26e4ae48b47c056cec3df42941b0c22216
NaimaOula <n.coton@gmail.com>
2022-03-08 13:14:48 +0100

    updated core/Dao.php with functions edit and delete

--- core/Dao.php
+++ core/Dao.php
@@ -17,7 +17,7 @@ final class Dao
      */
     public static function connect()
     {
-        $db_setting = parse_ini_file(__DIR__.'/../config.ini');
+        $db_setting = parse_ini_file(__DIR__ . '/../config.ini');
 
         try {
             self::$cnx = new \PDO(
@@ -44,20 +44,17 @@ final class Dao
 
         $sql = "SELECT * From {$classNameLower} WHERE ";
 
-        foreach (array_keys($args) as $key => $value)
-        {
-            $sql .= $value.' = :'.$value;
-            if($key < count($args)- 1)
-            {
-                $sql .=' AND ';
+        foreach (array_keys($args) as $key => $value) {
+            $sql .= $value . ' = :' . $value;
+            if ($key < count($args) - 1) {
+                $sql .= ' AND ';
             }
         }
 
         $stmt = self::$cnx->prepare($sql);
 
-        foreach ($args as $key => $value)
-        {
-            $stmt->bindParam(':'.$key, $value);
+        foreach ($args as $key => $value) {
+            $stmt->bindParam(':' . $key, $value);
 
         }
 
@@ -73,30 +70,26 @@ final class Dao
      * @param array $args
      * @return mixed
      */
-    public static function getMany(string $className, array $args = []):mixed
+    public static function getMany(string $className, array $args = []): mixed
     {
         $classNameLower = strtolower($className);
         $classNameLower = explode("\\", $classNameLower);
         $classNameLower = end($classNameLower);
 
         $sql = "SELECT * From {$classNameLower}";
-        if(count($args) != 0)
-        {
+        if (count($args) != 0) {
             $sql .= ' WHERE ';
-            foreach (array_keys($args) as $key => $value)
-            {
-                $sql .= $value.' = :'.$value;
-                if($key < count($args)- 1)
-                {
-                    $sql .=' AND ';
+            foreach (array_keys($args) as $key => $value) {
+                $sql .= $value . ' = :' . $value;
+                if ($key < count($args) - 1) {
+                    $sql .= ' AND ';
                 }
             }
         }
         $stmt = self::$cnx->prepare($sql);
 
-        foreach ($args as $key => $value)
-        {
-            $stmt->bindParam(':'.$key, $value);
+        foreach ($args as $key => $value) {
+            $stmt->bindParam(':' . $key, $value);
 
         }
 
@@ -116,17 +109,16 @@ final class Dao
     public static function insertOne(object $object, array $object_to_array): int
     {
         $exploded_namespace = explode('\\', $object::class);
-        $className= '`'.strtolower(end($exploded_namespace)).'`';
+        $className = '`' . strtolower(end($exploded_namespace)) . '`';
 
-        $propreties =[];
-        $values=[];
-        foreach ($object_to_array as $key => $value)
-        {
-            $propreties[] = '`'.$key.'`';
-            $values[] = ':'.$key;
+        $propreties = [];
+        $values = [];
+        foreach ($object_to_array as $key => $value) {
+            $propreties[] = '`' . $key . '`';
+            $values[] = ':' . $key;
         }
         $propreties_to_string = implode(" , ", $propreties);
-        $values_to_string =  implode(" , ", $values);
+        $values_to_string = implode(" , ", $values);
         $sql = "INSERT INTO {$className} ({$propreties_to_string}) VALUES ( {$values_to_string} )";
 
         $stmt = self::$cnx->prepare($sql);
@@ -136,14 +128,59 @@ final class Dao
         return self::$cnx->lastInsertId();
     }
 
-    // TODO
-    public static function edit()
+    /**
+     * @param string $className le nom de la classe à modifier
+     * @param array $args les arguments à modifier
+     * @param array $criteria la condition de l'élément à modifier, il faut que ça soit l'identifiant
+     * @return bool retoune true si la modification a été bien effectué et false dans le cas contraire
+     */
+    public static function edit(string $className, array $args, array $criteria) : bool
     {
+        $classNameLower = strtolower($className);
+        $classNameLower = explode("\\", $classNameLower);
+        $classNameLower = end($classNameLower);
+
+        $sql = "UPDATE {$classNameLower} SET";
+
+        foreach (array_keys($args) as $key => $value) {
+            $sql .= $value . ' = :' . $value;
+            if ($key < count($args) - 1) {
+                $sql .= ' , ';
+            }
+        }
+
+        $sql .= ' WHERE '.array_key_first($criteria)." = :".array_key_first($criteria);
+        /**
+         * @var \PDOStatement $stmt
+         */
+        $stmt = self::$cnx->prepare($sql);
+        return $stmt->execute(array_merge($args, $criteria));
 
     }
-     public static function delete()
-    {
 
+    /**
+     * @param string $className nom de la classe
+     * @param array $args les critères de l'élément à supprimer
+     * @return bool true si la suppression a été effectuée si non false
+     */
+    public static function delete(string $className, array $args): bool
+    {
+        $classNameLower = strtolower($className);
+        $classNameLower = explode("\\", $classNameLower);
+        $classNameLower = end($classNameLower);
+
+        $sql = "DELETE FROM `{$classNameLower}` WHERE ";
+
+        foreach (array_keys($args) as $key => $value) {
+            $sql .= $value . ' = :' . $value;
+            if ($key < count($args) - 1) {
+                $sql .= ' AND ';
+            }
+        }
+
+        $stmt = self::$cnx->prepare($sql);
+
+        return $stmt->execute($args);
     }
 
 }
