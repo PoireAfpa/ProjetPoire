8404e390312132df3ba9cb9214433f5dbfb0a113
NaimaOula <n.coton@gmail.com>
2022-03-09 12:42:53 +0100

    routes creation

--- public/index.php
+++ public/index.php
@@ -1,23 +1,19 @@
 <?php
-// test 2 naima
+
 require_once __DIR__ .'/../vendor/autoload.php';
 
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
-
 // TODO organiser la partie configuration
 $config = parse_ini_file(__DIR__ . "/../config.ini");
 define('BASE_URI', $config['base_uri']);
-
-
 
 // Router
 use App\core\Router;
 
 $router = new Router();
-
 
 
 /************ /Routes *************/
@@ -27,11 +23,21 @@ $router->register('/login', '\App\controller\UsersController::afficherLogin');
 $router->register('/logout', '\App\controller\UsersController::afficherLogout');
 $router->register('/dashboard', '\App\controller\UsersController::afficherDashboard');
 $router->register('/contact', '\App\controller\UsersController::afficherTest');
-$router->register('/dahsboard/add-client', '\App\controller\ClientsController::addClient');
-$router->register('/dahsboard/list-client', '\App\controller\ClientsController::list');
+$router->register('/dahsboard/user/list', '\App\controller\UsersController::list');
+$router->register('/dahsboard/user/add', '\App\controller\UsersController::add');
 $router->register('/dahsboard/user/edit/#iduser', '\App\controller\UsersController::edit');
+$router->register('/dahsboard/user/delete/#iduser', '\App\controller\UsersController::delete');
+$router->register('/dahsboard/client/list', '\App\controller\ClientsController::list');
+$router->register('/dahsboard/client/add', '\App\controller\ClientsController::add');
+$router->register('/dahsboard/client/edit/#idclient', '\App\controller\ClientsController::edit');
+$router->register('/dahsboard/client/delete/#idclient', '\App\controller\ClientsController::delete');
+$router->register('/dahsboard/projet/list', '\App\controller\ProjetsController::list');
+$router->register('/dahsboard/projet/add', '\App\controller\ProjetsController::add');
+$router->register('/dahsboard/projet/edit/#idprojet', '\App\controller\ProjetsController::edit');
+$router->register('/dahsboard/projet/delete/#idprojet', '\App\controller\ProjetsController::delete');
+$router->register('/dahsboard/contact/list', '\App\controller\ContactsController::list');
+$router->register('/dahsboard/contact/add', '\App\controller\ContactsController::add');
+$router->register('/dahsboard/contact/edit/#idcontact', '\App\controller\ContactsController::edit');
+$router->register('/dahsboard/contact/delete/#idcontact', '\App\controller\ContactsController::delete');
+
 $router->run();
-
-
-//test
-//test 2
