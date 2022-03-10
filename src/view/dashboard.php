e87d4ad439b4bb44a7094738abf28f36196a3870
Alex Gaoua <alexg57000@gmail.com>
2022-03-10 21:08:25 +0100

    Modif dashboard
    Petit changement pour afficher la photo du contact au lieu de l'url

--- src/view/dashboard.php
+++ src/view/dashboard.php
@@ -674,7 +674,7 @@ foreach($contacts as $contact){
    $html.='<td class= "text-center align-middle">'.$contact->getPrenomContact().'</td>';
    $html.='<td class= "text-center align-middle">'.$contact->getTelContact().'</td>';
    $html.='<td class= "text-center align-middle">'.$contact->getEmailContact().'</td>';
-   $html.='<td class= "text-center align-middle">'.$contact->getPhoto().'</td>';
+   $html.='<td class= "text-center align-middle"><img src='.$contact->getPhoto().'></td>';
    $html.='<td class= "text-center align-middle">'.$contact->getDuree().'</td>';
    $html.=' <td><button  data-id="'.$contact->getIdcontact().'" data-login="'.$contact->getNomContact().'" type="submit" name="editCntct" data-bs-target="#modalEditCntct'.$contact->getIdContact().'" data-bs-toggle="modal" class="btn-sm btn btn-success">Modifier</button></td>
    <td><input name="delCntct"  value="Supprimer" type="submit" class="btn-sm btn btn-danger test" data-bs-toggle="modal" data-bs-target="#modalDelCntct"/></a></td>';
